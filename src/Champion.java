c3961721cc9391a7ebe5c9adbaebdff333af7551
Arnau <arnauvivescomes@gmail.com>
2021-10-17 17:29:35 +0200

    Entrega

---
+++ src/Champion.java
@@ -0,0 +1,65 @@
+public class Champion {
+    private static int count=0;
+    private int id;
+    private String name;
+    private String title;
+    private String role;
+    private double difficulty;
+    private int skins;
+    private boolean reworked;
+    private String url;
+
+    public Champion(String name, String title, String role, double difficulty, int skins, boolean reworked, String url){
+        this.id=count;
+        count++;
+        this.name=name;
+        this.title=title;
+        this.role=role;
+        this.difficulty=difficulty;
+        this.skins=skins;
+        this.reworked=reworked;
+        this.url=url;
+    }
+   static public Champion fromLine (String line){
+
+        String part[] = line.split(",");
+
+        return new Champion(part[0], part[1], part[2],  Double.parseDouble(part[3]),Integer.parseInt(part[4]), Boolean.parseBoolean(part[5]), part[6]);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public double getDifficulty() {
+        return difficulty;
+    }
+
+    public int getSkins() {
+        return skins;
+    }
+
+    public boolean isReworked() {
+        return reworked;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setReworked(boolean reworked) {
+        this.reworked = reworked;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,103 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+// declaracion de variables que voy a usar durante el programa
+        String text="", role;
+        int menu, sec_menu, pop_champ_count ;
+        double rework_sum;
+        boolean role_exists;
+        ArrayList<Champion> championlist = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+
+// Leer Fichero-----------------------------------------------------------------------------------------------------------------------------------------
+        try {
+            text = new String(Files.readAllBytes(Path.of("champions.csv")));
+        } catch (IOException e) {
+            System.out.println("Error while opening the file.");
+            e.printStackTrace();
+        }
+       // System.out.println(text);
+
+// Separar y meter champions en la lista-------------------------------------------------------------------------------------------
+
+        for(String linea : text.split("\n")){
+            Champion aux = Champion.fromLine(linea);
+            //System.out.println(aux.getId()+aux.getName()+aux.getDifficulty()+ aux.getRole()+ aux.getTitle()+ aux.getSkins()+aux.isReworked());
+            championlist.add(aux);
+        }
+
+//Menu-----------------------------------------------------------------------------------------------------------------------------------------
+        try {
+            do {
+                rework_sum = 0;
+                pop_champ_count=0;
+                role_exists=false;
+
+                System.out.println("--Champions--\n1.Rework champion\n2.Show reworked\n3.Show popular difficulty\n4.Show images by role\n5.Exit\n\nEnter an option: ");
+                menu = sc.nextInt();
+
+                if (menu>0&&menu<6){
+                    switch (menu) {
+                        case 1:
+                            try {
+                                System.out.println("Enter an identifier: ");
+                                sec_menu = sc.nextInt();
+                                if(sec_menu<championlist.size() && sec_menu>0){
+                                    for (Champion champion : championlist) {
+                                        if (champion.getId() == sec_menu) {
+                                            champion.setReworked(true);
+                                            System.out.println("Reworked: " + champion.getId() + "- " + champion.getName() + ", " + champion.getTitle());
+                                        }
+                                    }
+                                }else System.out.println("Enter an id that belongs to a champion (your id is too low or too high)");
+                            } catch (InputMismatchException e) {
+                                System.out.println("Insert a correct option");
+                                e.printStackTrace();
+                            }
+                            break;
+                        case 2:
+                            for (Champion champion : championlist) {
+                                if (champion.isReworked()) {
+                                    System.out.println(champion.getId()+"- "+champion.getName()+", "+champion.getTitle());
+                                }
+                            }
+                            break;
+                        case 3:
+                            for (Champion champion : championlist) {
+                                if (champion.getSkins() >= 10) {
+                                    rework_sum += champion.getDifficulty();
+                                    pop_champ_count++;
+                                }
+                            }
+                            System.out.println("Average difficulty for popular champions: " + rework_sum / pop_champ_count);
+                            break;
+                        case 4:
+                            System.out.println("Enter a role: ");
+                            role = sc.next();
+                            for (Champion champion : championlist) {
+                                if (champion.getRole().compareTo(role) == 0) {
+                                    System.out.println(champion.getUrl());
+                                    role_exists=true;
+                                }
+                            }
+                            if(!role_exists){
+                                System.out.println("Enter a role that exists");
+                            }
+                            break;
+                    }
+                }else System.out.println("Enter a correct option.");
+                System.out.println();
+            } while (menu != 5);
+
+        }catch (InputMismatchException e){
+            System.out.println("Insert a correct option\n");
+            e.printStackTrace();
+        }
+        System.out.println("Bye!\n");
+    }
+}
